c1b5bfd81cb37a6692afbf0030aa542665b2766c
Markos Fragkopoulos <exog3n@gmail.com>
2020-06-23 09:11:31 +0300

    development stage: rpi setuped with one time uplink childprocess, front app collect coords

---
+++ rpi/nodebot/artest.js
@@ -0,0 +1,35 @@
+// const webServer = require('./webserver.js');
+// let sockets = webServer.webServer();
+
+//
+const SerialPort = require('serialport');
+const Readline = require('@serialport/parser-readline');
+const port_uno_drag = new SerialPort('/dev/ttyUSB0' ,{ baudRate: 9600 }, function (err) {
+  if (err) {
+    return console.log('Error: ', err.message)
+  }
+});
+const parser_uno_drag = port_uno_drag.pipe(new Readline({ delimiter: '\n' }));
+
+// let msg = new Buffer('test\n', "binary");
+let msg = Buffer.from('test\n', 'utf8');
+
+parser_uno_drag.write(msg, (err) => {
+  if (err) {
+    return console.log('Error on write: ', err.message);
+  }
+});
+
+parser_uno_drag.on('error', function(err) {
+  console.log('Error: ', err.message)
+})
+
+// Read data that is available but keep the stream in "paused mode"
+parser_uno_drag.on('readable', function () {
+  console.log('Data:', port.read())
+})
+
+// Switches the port into "flowing mode"
+parser_uno_drag.on('data', function (data) {
+  console.log('Data:', data)
+})
